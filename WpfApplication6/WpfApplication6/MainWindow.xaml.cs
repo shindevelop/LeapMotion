0b0e6c7ae23b699f40a9167bd0a74a808802dd9a
Kohei Shindow <leziption@gmail.com>
2013-07-26 13:41:29 +0900

    v1
    created listener enables start on WPF

---
+++ WpfApplication6/WpfApplication6/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+
+using Leap;
+
+namespace WpfApplication6
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Controller ctr;
+        WPFListener lst;
+        Ellipse elp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                //センサーとリスナの定義
+                lst = new WPFListener();
+
+                ctr = new Controller(lst);
+                ctr.EnableGesture(Gesture.GestureType.TYPECIRCLE);
+                //ctr.AddListener(lst);
+                lst.LeapFrameReady += lst_LeapFrameReady;
+
+                elp = new Ellipse() { Width = 20, Height = 20, Stroke = Brushes.Red, StrokeThickness = 3, Fill = Brushes.Red };
+                cvs.Children.Add(elp);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButton.OK);
+                Application.Current.Shutdown();
+            }
+        }
+
+        void lst_LeapFrameReady(object sender, LeapFrameReadyEventArgs e)
+        {
+            if (!e.Frame.Fingers.IsEmpty)
+            {
+                Finger f = e.Frame.Fingers[0];
+
+                //メインスレッド処理はココ
+                Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    elp.Width = Math.Abs(160 - f.TipPosition.z);
+                    elp.Height = Math.Abs(160 - f.TipPosition.z);
+
+                    Canvas.SetLeft(elp, (this.ActualWidth / 2) + f.TipPosition.x);
+                    Canvas.SetTop(elp, (this.ActualHeight / 2) - f.TipPosition.y);
+
+                    LblX.Content = f.TipPosition.x.ToString();
+                    LblY.Content = f.TipPosition.y.ToString();
+                    LblZ.Content = f.TipPosition.z.ToString();
+                }));
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ctr.RemoveListener(lst);
+            ctr.Dispose();
+            lst.Dispose();
+        }
+    }
+}
---
+++ WpfApplication6/WpfApplication6/WPFListener.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace WpfApplication6
+{
+    class WPFListener : Listener
+    {
+        public event EventHandler<LeapFrameReadyEventArgs> LeapFrameReady;
+
+        /// <summary>
+        /// リスナー初期化
+        /// </summary>
+        /// <param name="ctr"></param>
+        public override void OnInit(Controller ctr)
+        {
+
+        }
+
+        /// <summary>
+        /// 接続時
+        /// </summary>
+        /// <param name="ctr"></param>
+        public override void OnConnect(Controller ctr)
+        {
+            if (!ctr.IsConnected)
+            {
+                Console.WriteLine("Leap is not connected");
+            }
+            else
+            {
+                Console.WriteLine("Leap is connected");
+            }
+        }
+
+        /// <summary>
+        /// 取り外し時
+        /// </summary>
+        /// <param name="ctr"></param>
+        public override void OnDisconnect(Controller ctr)
+        {
+            Console.WriteLine("Leap was disconnected!");
+        }
+
+        /// <summary>
+        /// 単位フレーム
+        /// </summary>
+        /// <param name="ctr">Leap</param>
+        public override void OnFrame(Controller ctr)
+        {
+            Frame frame = ctr.Frame();
+
+            if (LeapFrameReady != null)
+            {
+                LeapFrameReady(this, new LeapFrameReadyEventArgs(frame));
+            }
+        }
+
+        /// <summary>
+        /// コントローラからリスナが削除・外された時
+        /// </summary>
+        /// <param name="ctr"></param>
+        public override void OnExit(Controller ctr)
+        {
+            
+        }
+    }
+
+    class LeapFrameReadyEventArgs : EventArgs
+    {
+        public Frame Frame { get; set; }
+
+        public LeapFrameReadyEventArgs(Frame frame)
+        {
+            Frame = frame;
+        }
+    }
+}
